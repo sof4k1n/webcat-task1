4113f58d18c73db37b4f02f6bbd9e931dca9e20e
Sofakin <54062133+sof4k1n@users.noreply.github.com>
2024-10-31 17:15:09 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'htmx.org';
+import './styles/main.scss'
+import hlogo from './images/hlogo.svg'
+import flogo from './images/flogo.svg'
+
+window.htmx = require('htmx.org');
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { type } = require('os');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    filename: path.resolve(__dirname, 'src/index.js') // путь на входной файл
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'), // путь на выходной файл
+    filename: 'index.js', // Имя выходного файла сборки
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
+      {
+        test: /\.(ttf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    port: 8000,
+    compress: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    open: true,
+  }
+};
