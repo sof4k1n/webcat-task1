99a9c03d98cd65969f989858d03d2d79042c24da
Sofakin <54062133+sof4k1n@users.noreply.github.com>
2024-11-07 01:31:05 +0500

    Финальная версия
    Первая сверстаная страничка (https://www.figma.com/file/p5xYvZV5Vy9...e-id=0%3A1)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import 'htmx.org';
-import './styles/main.scss'
-import hlogo from './images/hlogo.svg'
-import flogo from './images/flogo.svg'
-import adres from './images/adres.svg'
-import phone from './images/phone.svg'
-import picture1 from './images/picture1.png'
-import picture2 from './images/picture2.png'
-import picture3 from './images/picture3.png'
-import picture4 from './images/picture4.png'
+import './styles/main.scss';
+import hlogo from './images/hlogo.svg';
+import flogo from './images/flogo.svg';
+import adres from './images/adres.svg';
+import phone from './images/phone.svg';
+import picture1 from './images/picture1.png';
+import picture2 from './images/picture2.png';
+import picture3 from './images/picture3.png';
+import picture4 from './images/picture4.png';
+import check from './images/check.svg';
 
-window.htmx = require('htmx.org');
+
+// --- Функция с лоадером
+function set_loader(event) {
+    const button = event.target; // Получаем кнопку, которая инициировала событие
+    button.classList.add("loader"); // Добавляем лоадер на кнопку
+}
+
+// --- Функция для применения окончательного вида кнопки
+function buy_complite(event) {
+    const button = event.target; // Получаем кнопку, которая инициировала событие
+    const heading = button.querySelector('h4'); // Текст который нужно изменить
+
+    button.classList.remove("loader");
+    button.classList.add("inCart"); // Добавляем стиль "В корзине"
+    heading.innerText = 'В корзине'; // Меняем текст
+    
+    // Сохраняем состояние в локальном хранилище
+    localStorage.setItem(button.dataset.buttonId, 'true');
+}
+
+function remove_loader(button) {
+    button.classList.remove('loader'); // Удаляем лоадер
+    button.classList.remove('inCart'); // Убираем стиль "В корзине"
+    button.querySelector('h4').innerText = 'Купить'; // Возвращаем текст
+}
+
+// Восстанавливаем состояние кнопок из локального хранилища
+document.querySelectorAll(".btn").forEach(button => {
+    const buttonId = button.dataset.buttonId;
+    const storedState = localStorage.getItem(buttonId);
+
+    if (storedState) {
+        buy_complite({ target: button }); // Применяем окончательный вид кнопки
+    }
+
+    // Обработчик события для "htmx:beforeRequest" (Лоадер) и "htmx:afterRequest" (Конечный вид кнопки)
+    button.addEventListener("htmx:beforeRequest", set_loader);
+    button.addEventListener("htmx:afterRequest", (event) => {
+        if (event.detail.xhr.status === 200) {
+            buy_complite(event); // Если запрос успешен, применяем стиль "В корзине"
+            console.log("Успешный запрос")
+        } else {
+            remove_loader(button); // Если не успешен, возвращаем обычную кнопку
+            console.log("Неудачный запрос")
+        }
+    });
+});
